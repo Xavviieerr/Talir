a3fc447fe6b1d326ddc11a289c0c148015578387
Xavviieerr <ogbupaul545@gmail.com>
2025-10-17 12:40:23 +0100

    structure creation

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Home } from "../pages/Home";
+// import { Dashboard } from "../pages/Dashboard";
+// import { Login } from "../features/auth/Login";
+// import { Navbar } from "../components/layout/Navbar";
+
+// export const AppRoutes = () => (
+//   <BrowserRouter>
+//     <Navbar />
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/dashboard" element={<Dashboard />} />
+//       <Route path="/login" element={<Login />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
